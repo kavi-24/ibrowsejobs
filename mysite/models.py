876b846f11fd7b078b1086f39ad4cc019b5f1385
kavi-24 <srikavipriyan.ma2021@vitstudent.ac.in>
2023-08-20 10:03:02 +0530

    initial commit

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=40)
+    password = models.CharField(max_length=50)
